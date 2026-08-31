ef49115bcb22ee173bde10bc3c9ebf72bf3a9e77
Richa-2309 <165482415+Richa-2309@users.noreply.github.com>
2026-08-31 23:53:57 +0530

    Correct middleware function names and comments
    Fix typos in middleware function names and comments.

--- express-server.js
+++ express-server.js
@@ -1,15 +1,12 @@
 const express=require('express');
 const server =express();
 // this is my controller functiom
-
 function globalmidilware(req,res,next){
     console.log("this is global midilware");
     next()
 }
 server.use(globalmidilware);
 server.use(express.static("public"))
-
-
 //miderware as function
 function firstmidilware(req,res,next){
     console.log("this is the first midilware")
@@ -30,6 +27,7 @@ server.get("/",(req,res)=>{
 server.post("/",(req,res)=>{
     res.send("this is root server for post request and it is working");
 })
+
 server.put("/",(req,res)=>{
     res.send("this is root server for put request and it is working");
 })
@@ -37,9 +35,11 @@ server.delete("/",(req,res)=>{
     res.send("this is root server for delete request and it is working");
 })
 
+
 server.get("/send",firstmidilware, Secondmidilware,(req,res)=>{
     res.send("send route server is working fine as expected");
 })
+
 
 server.listen(8080,()=>{
     console.log("server is listening");
