1263f079aea85059cb579b32ce8968de559733a3
Richa-2309 <165482415+Richa-2309@users.noreply.github.com>
2026-09-06 23:07:01 +0530

    Refactor express server with middleware updates

--- express-server.js
+++ express-server.js
@@ -1,49 +1,63 @@
-const express=require('express');
-const server =express();
-// this is my controller functiom
-function globalmidilware(req,res,next){
-    console.log("this is global midilware");
-    next()
-}
-server.use(globalmidilware);
-server.use(express.static("public"))
-//miderware as function
-function firstmidilware(req,res,next){
-    console.log("this is the first midilware")
-    next()
-}
-function Secondmidilware(req,res,next){
-    console.log("this is the second midilware")
-    next()
-}
-//miderware as function
-function thirdmidilware(req,res,next){
-    console.log("this is the third midilware")
-    next()
-}
-server.get("/",(req,res)=>{
-    res.send("this is root server and it is working");
-})
-server.post("/",(req,res)=>{
-    res.send("this is root server for post request and it is working");
-})
+const express = require("express");
 
-server.put("/",(req,res)=>{
-    res.send("this is root server for put request and it is working");
-})
-server.delete("/",(req,res)=>{
-    res.send("this is root server for delete request and it is working");
-})
+const server = express();
 
+// Global Middleware
+server.use((req, res, next) => {
+    console.log("Global Middleware");
+    next();
+});
 
-server.get("/send",firstmidilware, Secondmidilware,(req,res)=>{
-    res.send("send route server is working fine as expected");
-})
-server.get("/send2",firstmidilware, Secondmidilware,(req,res)=>{
-    res.send("send route server is working fine as expected");
-})
+server.use(express.static("public"));
 
+// Route Middleware
+const firstMiddleware = (req, res, next) => {
+    console.log("First Middleware");
+    next();
+};
 
-server.listen(8080,()=>{
-    console.log("server is listening");
-})
+const secondMiddleware = (req, res, next) => {
+    console.log("Second Middleware");
+    next();
+};
+
+// Root Routes
+server.get("/", (req, res) => {
+    res.send("GET request is working");
+});
+
+server.post("/", (req, res) => {
+    res.send("POST request is working");
+});
+
+server.put("/", (req, res) => {
+    res.send("PUT request is working");
+});
+
+server.delete("/", (req, res) => {
+    res.send("DELETE request is working");
+});
+
+// Routes with Middleware
+server.get(
+    "/send",
+    firstMiddleware,
+    secondMiddleware,
+    (req, res) => {
+        res.send("Send route is working");
+    }
+);
+
+server.get(
+    "/send2",
+    firstMiddleware,
+    secondMiddleware,
+    (req, res) => {
+        res.send("Send2 route is working");
+    }
+);
+
+// Start Server
+server.listen(8080, () => {
+    console.log("Server is listening on port 8080");
+});
