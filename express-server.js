5da341beaccb061896625c4957f3baa8b1b15cf7
Richa-2309 <165482415+Richa-2309@users.noreply.github.com>
2026-09-08 23:25:41 +0530

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const server = express();
+const PORT = 8080;
 
 // Global Middleware
 server.use((req, res, next) => {
@@ -10,7 +11,7 @@ server.use((req, res, next) => {
 
 server.use(express.static("public"));
 
-// Route Middleware
+// Route Middlewares
 const firstMiddleware = (req, res, next) => {
     console.log("First Middleware");
     next();
@@ -22,42 +23,25 @@ const secondMiddleware = (req, res, next) => {
 };
 
 // Root Routes
-server.get("/", (req, res) => {
-    res.send("GET request is working");
+server
+    .route("/")
+    .get((req, res) => res.send("GET request is working"))
+    .post((req, res) => res.send("POST request is working"))
+    .put((req, res) => res.send("PUT request is working"))
+    .delete((req, res) => res.send("DELETE request is working"));
+
+// Routes with common middleware
+const commonMiddleware = [firstMiddleware, secondMiddleware];
+
+server.get("/send", ...commonMiddleware, (req, res) => {
+    res.send("Send route is working");
 });
 
-server.post("/", (req, res) => {
-    res.send("POST request is working");
+server.get("/send2", ...commonMiddleware, (req, res) => {
+    res.send("Send2 route is working");
 });
-
-server.put("/", (req, res) => {
-    res.send("PUT request is working");
-});
-
-server.delete("/", (req, res) => {
-    res.send("DELETE request is working");
-});
-
-// Routes with Middleware
-server.get(
-    "/send",
-    firstMiddleware,
-    secondMiddleware,
-    (req, res) => {
-        res.send("Send route is working");
-    }
-);
-
-server.get(
-    "/send2",
-    firstMiddleware,
-    secondMiddleware,
-    (req, res) => {
-        res.send("Send2 route is working");
-    }
-);
 
 // Start Server
-server.listen(8080, () => {
-    console.log("Server is listening on port 8080");
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
